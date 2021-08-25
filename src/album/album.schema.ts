e7a8b2433985150eb546ef89245c750b3ff6fc41
Andrey_B <example@gmail.com>
2021-08-25 17:07:14 +0300

    procfile for deploy

---
+++ src/album/album.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import {Track} from "../track/schemas/track.schema";
+import * as mongoose from 'mongoose'
+
+export type AlbumDocument = Album & Document;
+
+@Schema()
+export class Album {
+    @Prop()
+    name: string;
+
+    @Prop()
+    author: string;
+
+    @Prop()
+    picture: string;
+
+    @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Track'})
+    tracks: Track[];
+    
+}
+
+export const AlbumSchema = SchemaFactory.createForClass(Album);
---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { FileModule } from "./file/file.module";
+import { TrackModule } from "./track/track.module";
+import * as path from 'path'
+
+@Module({
+    imports: [
+        ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static')}),
+        MongooseModule.forRoot('mongodb+srv://admin:N8FJMMUgvej7yqKl@cluster0.rzrdp.mongodb.net/music-platform?retryWrites=true&w=majority'),
+        TrackModule,
+        FileModule
+    ]
+})
+export class AppModule {}
